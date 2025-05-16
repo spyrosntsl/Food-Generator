1429cc2d438c7c9a4ed3958f9bfafe0653cd951e
Spyros Ns <ntislisspyros@gmail.com>
2025-05-16 20:41:09 +0300

    Update FooD.js

--- FooD.js
+++ FooD.js
@@ -1,17 +1,16 @@
-// app.js
 const btn = document.getElementById("btn-1");
 btn.addEventListener("click", fetchRandomRecipe);
 
 async function fetchRandomRecipe() {
   try {
-    const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const res  = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
     const data = await res.json();
     const meal = data.meals[0];
 
-    document.getElementById("recipe-name").textContent = meal.strMeal;
+    document.getElementById("recipe-name").textContent  = meal.strMeal;
     document.getElementById("recipe-steps").textContent = meal.strInstructions;
   } catch (err) {
-    console.error(err)
-    alert("something went wrong try again");
+    console.error(err);
+    alert("Something went wrong. Try again.");
   }
 }
