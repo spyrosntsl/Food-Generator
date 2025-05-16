6a64b2e18025a89e00c3b5808454fbc41a5310c0
NS HooD <ntislisspyros@gmail.com>
2025-05-16 18:40:17 +0300

    Initial commit – static food app

---
+++ FooD.js
@@ -0,0 +1,17 @@
+// app.js
+const btn = document.getElementById("btn-1");
+btn.addEventListener("click", fetchRandomRecipe);
+
+async function fetchRandomRecipe() {
+  try {
+    const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const data = await res.json();
+    const meal = data.meals[0];
+
+    document.getElementById("recipe-name").textContent = meal.strMeal;
+    document.getElementById("recipe-steps").textContent = meal.strInstructions;
+  } catch (err) {
+    console.error(err);
+    alert("Κάτι πήγε στραβά, ξαναδοκίμασε!");
+  }
+}
